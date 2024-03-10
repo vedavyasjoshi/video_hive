2f6872ddecb0989cf6615b59ee9074e38d4c3b50
Vedavyas <vedavyasjoshi121@gmail.com>
2024-03-10 18:24:01 +0530

    Clean up code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,6 @@
-
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&key=" +
-  YOUTUBE_API_KEY +
+  process.env.REACT_APP_YOUTUBE_API_KEY +
   "&pageToken=";
 
 export const YOUTUBE_SUGGEST_API =
@@ -9,7 +8,7 @@ export const YOUTUBE_SUGGEST_API =
 
 export const YOUTUBE_SEARCH_API =
   "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=" +
-  YOUTUBE_API_KEY +
+  process.env.REACT_APP_YOUTUBE_API_KEY +
   "&q=";
 
 export const USER_IMAGE_URL =
