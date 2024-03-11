0823cc25722cba4648a79b5d41e20e5088006688
Vedavyas <vedavyasjoshi121@gmail.com>
2024-03-11 08:51:44 +0530

    Fix http to https in suggestion api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ export const YOUTUBE_VIDEOS_API =
   "&pageToken=";
 
 export const YOUTUBE_SUGGEST_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const YOUTUBE_SEARCH_API =
   "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=" +
