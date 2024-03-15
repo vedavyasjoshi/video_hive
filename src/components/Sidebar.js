731ff13c03f569b63fc457bf28327eda8275d22f
Vedavyas <vedavyasjoshi121@gmail.com>
2024-03-15 08:39:57 +0530

    Iconify sidebar items

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import lang from "../utils/languageConstants";
+import icons from "../utils/icons";
 
 const Sidebar = () => {
   const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
@@ -15,22 +16,39 @@ const Sidebar = () => {
       <div className="p-5 shadow-lg w-48 min-w-44">
         <ul>
           <li>
-            <Link to="/" className="font-bold">{lang[langKey]["home"]}</Link>
+            <Link to="/" className="font-bold">
+              {lang[langKey]["home"]}
+            </Link>
           </li>
           {videoTypes.map((item) => (
-            <li key={item}>{lang[langKey][item] || item}</li>
+            <li
+              key={item}
+              className="cursor-pointer px-2 py-1 hover:border-black hover:bg-gray-200 rounded-md"
+            >
+              {icons[item]} {lang[langKey][item] || item}
+            </li>
           ))}
         </ul>
         <h1 className="font-bold pt-5">{lang[langKey]["subscriptons"]}</h1>
         <ul>
           {subscriptions.map((item, ind) => (
-            <li key={item+ind}>{lang[langKey][item] || item}</li>
+            <li
+              key={item + ind}
+              className="cursor-pointer px-2 py-1 hover:border-black hover:bg-gray-200 rounded-md"
+            >
+              {icons[item]} {lang[langKey][item] || item}
+            </li>
           ))}
         </ul>
         <h1 className="font-bold pt-5">{lang[langKey]["watch later"]}</h1>
         <ul>
-          {subscriptions.map((item,ind) => (
-            <li key={item+ind}>{lang[langKey][item] || item}</li>
+          {subscriptions.map((item, ind) => (
+            <li
+              key={item + ind}
+              className="cursor-pointer px-2 py-1 hover:border-black hover:bg-gray-200 rounded-md"
+            >
+              {icons[item]} {lang[langKey][item] || item}
+            </li>
           ))}
         </ul>
       </div>
---
+++ src/utils/icons.js
@@ -0,0 +1,11 @@
+const icons = {
+  shorts: "📽️",
+  videos: "🎥",
+  live: "🔴",
+  music: "🎵",
+  sports: "⚽️",
+  gaming: "🎮",
+  movies: "🎬",
+};
+
+export default icons;
