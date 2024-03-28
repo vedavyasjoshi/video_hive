1dc69958f9ad615c8f0fc8cbea0c062904139050
Vedavyas <vedavyasjoshi121@gmail.com>
2024-03-28 09:25:23 +0530

    Custom video-hive logo

--- src/components/Head.js
+++ src/components/Head.js
@@ -10,6 +10,7 @@ import {
 import { cacheResults } from "../utils/searchSlice";
 import { setVideoRecords } from "../utils/videoSlice";
 import { changeLanguage } from "../utils/configSlice";
+import applogo from "../assets/applogo.png";
 
 const Head = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -77,19 +78,20 @@ const Head = () => {
 
   return (
     <div className="grid grid-flow-col p-2 m-2 shadow-lg">
-      <div className="flex col-span-1">
+      <div className="flex col-span-1 items-center">
         <img
           className="h-8 cursor-pointer"
           alt="hamburger"
           src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAb1BMVEX///8iICGSkZIkHyEPDA3z8vIyMTEhGx40MDEeHB4jICEeHR4AAAAxMTEgHh4gHB3W1tYtKyyXlpe6uroKBQhzcnJ+fX7CwsKysrJmZWX19fXk5OQYFhc5ODgoJidta2xUVFRfXV7Kysqsq6yjo6MHDa+eAAAB8UlEQVR4nO3c3VLaQBgGYJY/IQtE1Iogrfbn/q+xCaQ2TqtFm222+jwHDC8MMO8EdjnY+QYDAAAAAAAAAAAAAAAAeI/OL4Z5uDhP0m+yXYwzcbX4cJug4d045GN8Pem84GYd+67VUq6/dN7wou9Sjy1u0jQcjUZ9V2skaHhZfUuLbBrGYtN5w8F2HLNpGFOsNIPddlo3XGUgTK9T7BbVFzWbHX+zS1IQAAAAAAAAAABeZJKHVPXO76dHs9msul1OH+JfpOmr0ufuz15Wbhb78uzBvJzPWym2U/XU6Sk+lc6eTnEfv3Zf8PZjeTib2AihnYpwOJl5Qhp1kULY33d/1Pvbp9XTDcO/bhjGl503HD5uUX/Mn1PxTPr964pTUkhygra+hj9U16V10LS6+/pUtFLxTAo/00GCa1j/DhtFDw2Lxw1T/A7rtTRWS+ZhES2rdS3O22lep/qBX1LZSmetFI+pfvzk1HximrW03g9ns4edadboIy2XafbDWt9/Zhqp6gEAAAAAAAAAwAu89Zl7u+00xFXse2ZiLdHcxO24PLx7DpLMvrxcHy9f3+WOUswvHYZVRg2TTNktqnqjTCa0Jmm4WZcZNUwxC3pwd5VPwyLJlN3JdnHV9zD2RqKZ7G9/rj4AAAAAAAAAAAAAAAD8T74DVhZG6MsBqOQAAAAASUVORK5CYII="
           onClick={toggleMenuHandler}
         />
-        <a href="/">
+        <a href="/" className="flex items-center">
           <img
             className="h-8 mx-2"
-            alt="youtube-logo"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlHMUb8U4VeW2y-RflH7U7Yp0tsx1hJv0PwQ&usqp=CAU"
+            alt="app-logo"
+            src={applogo}
           />
+          <h1 className="text-black font-bold">Video Hive</h1>
         </a>
       </div>
       <div className="col-span-10 px-10 ">
